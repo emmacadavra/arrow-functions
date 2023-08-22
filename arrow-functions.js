1e4f25e042b6b5f60bee3cd73d3900e4e57795e1
Emma Cadavra <emmacadavra@gmail.com>
2023-08-22 12:00:49 +0000

    Arrow functions lesson

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,37 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => {
+    return a + b;
+}
+let sum2 = addTwoNumbers2(4, 6);
+console.log(sum2);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers3 = (a, b) => a + b;
+
+let sum3 = addTwoNumbers3(5, 7);
+console.log(sum3);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething("Hello World!");
+
+const sayHello = () => console.log("Hello!")
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multi-line string!
+    </p>`
+)
+console.log(returnMultipleLines());
